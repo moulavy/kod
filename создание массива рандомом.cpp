045b501a251e136176601c90e49ed2e6b0d48e43
moulavy <72352010+moulavy@users.noreply.github.com>
2020-10-05 00:05:06 +0400

    добавила fstream
    добавила fstream

--- создание массива рандомом.cpp
+++ создание массива рандомом.cpp
@@ -1,47 +1,63 @@
-﻿// создание массива рандомом.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
 
 #include <iostream>
 #include <ctime>
+#include <algorithm>
+#include <fstream>
 using namespace std;
 int main()
 {
 	system("chcp 1251");
 	system("cls");
-	FILE* f;
-	int i = 0, n = 0, j, a[100000], x,v;
-	fopen_s(&f, "input.txt", "w+");
-	cout << "Заполнить массив самостоятельно или случайно?" << endl;
-	cout << "1. Самостоятельно." << endl;
-	cout << "2. Случайно." << endl;
-	cin >> v;
-	cout << "Введите количество элементов" << endl;
-	cin >> n;
-	if (v == 1)
+	fstream f,h;
+	int i = 0, n = 0, j, a[100000], x;
+	clock_t t1, t2;
+	t1 = clock();
+	char d;
+	f.open("input.txt", fstream::in);
+	h.open("output.txt", fstream::out);
+	if (f.is_open())
 	{
-		cout << "Введите элементы: " << endl;
-		for (i = 0; i < n; i++)
+		while (!f.eof())
 		{
-			cin >> a[i];
+			if (!f.eof())
+			{
+				f >> a[i++];
+				n++;
+			}
 		}
-		cout << endl<< "Массив введен." << endl;
 	}
-	else if (v == 2)
+	else
 	{
-		for (i = 0; i < n; i++)
-		{
-			a[i] = rand() % 10000 + 1;
-		}
-		cout << endl << "Массив заполнен случайными числами от 1 до 10000." << endl;
+		cout << "no" << endl;
 	}
-	for (i = 0; i < n-1; i++)
+	t2 = clock();
+	cout << "Время тиков(считывание из файла): " << t2 - t1 << endl;
+	cout << "Время в секундах(считывание из файла): " << double(t2 - t1) / CLOCKS_PER_SEC << endl;
+	//вывод исходного массива
+	for (i = 0; i < n; i++)
 	{
-		fprintf_s(f, "%d", a[i]);
-		fprintf_s(f, "%c", ' ');
+		cout << a[i] << " ";
 	}
-	fprintf_s(f, "%d", a[n-1]);
-	fclose(f);
+	cout << endl;
+
+	//cортировка
+	t1 = clock();
+	sort(a, a + n);
+	t2 = clock();
+	cout << endl << "Время тиков(сортировка): " << t2 - t1 << endl;
+	cout << "Время в секундах(сортировка): " << double(t2 - t1) / CLOCKS_PER_SEC << endl;
+
+	t1 = clock();
+	for (i = 0; i < n; i++)
+	{
+		h << a[i];
+		h << " ";
+	}
+	t2 = clock();
+	cout << endl << "Время тиков(запись в файл): " << t2 - t1 << endl;
+	cout << "Время в секундах(запись в файл): " << double(t2 - t1) / CLOCKS_PER_SEC << endl;
+	f.close();
+	h.close();
 	system("pause");
-	
 	return 0;
 }
